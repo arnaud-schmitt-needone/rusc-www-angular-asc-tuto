bd7e20ef80e9adbc2d39d69d56b40738edc904cc
Arnaud Schmitt <arnaud.schmitt@needone.fr>
2021-03-10 12:00:33 +0100

    fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + './dist/rusc-www-angular-asc-tuto'));
+app.use(express.static(__dirname + '/dist/rusc-www-angular-asc-tuto'));
 
 app.get('/*', (req, res) => {
     res.sendFile('index.html', { root: 'dist/rusc-www-angular-asc-tuto'});
@@ -9,4 +9,4 @@ app.get('/*', (req, res) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(this.port, console.log(`Server up ! Listening on port ${port} ...`));
+app.listen(port, console.log(`Server up ! Listening on port ${port} ...`));
