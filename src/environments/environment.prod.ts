9d5015f253815df3a3b7f2766092f221341b12b0
Arnaud S <arnaud.schmitt@needone.fr>
2021-03-11 17:05:24 +0100

    update environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'https://rusc-back-nestjs-asc-tuto.herokuapp.com/api',
   heroesEndpoint: '/heroes'
 };
